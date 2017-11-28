02f80bcbe40ffb606e76a47cc1c069b8bf9d373d
Miguel Montoya <miguel.mzbi@gmail.com>
2017-11-28 17:15:40 -0600

    Hash fix

--- bankhash.py
+++ bankhash.py
@@ -1,11 +1,8 @@
-# TODO - FIX HASHTABLE
-#       - Remmove must return deleted value
-#       - Insert must resize before inserting. Double the size when the HT is 75% full
-#       - Add method to see if id exists
 class BankHash:
     def __init__(self):
-        self.size = 25
-        self.buckets = [None] * self.size
+        self.capacity = 25
+        self.buckets = [None] * self.capacity
+        self.alpha = .75
 
     def __str__(self):
         toPrint = ""
@@ -13,32 +10,54 @@ class BankHash:
             if bucket != None:
                 toPrint += str(bucket) + "\n"
         return toPrint
-            
 
-    def put(self,item):
-        hashedkey = hash(item) % self.size
+    def exists(self, id):
+        start = hashedkey = hash(id) % self.capacity
+        done = False
+
+        while not done:
+            if self.buckets[hashedkey].getId() == id:
+                val = self.buckets[hashedkey]
+                return True
+            else:
+                hashedkey = (hashedkey + 1) % self.capacity
+                if hashedkey == start or self.buckets[hashedkey] is None:
+                    return False
+        return val
+
+    def resize(self):
+        l = len(self.buckets)//2
+        temp = self.buckets
+
+        for i in range(len(self.buckets)):
+            self.buckets[i] = None
+        for i in range(l):
+            self.buckets.append(None)
+
+        self.capacity += self.capacity
+        for element in temp:
+            self.put(element)
+
+    def put(self, item):
+        if len(self.buckets)/self.capacity >= self.alpha:
+            self.resize()
+
+        hashedkey = hash(item) % self.capacity
 
         if self.buckets[hashedkey] == None:
             self.buckets[hashedkey] = item
 
-        ##elif self.buckets[hashedkey] == value:
-        ##    self.values[hashedkey] = value
-
         else:
-            ##while(self.buckets[hashedkey] != None and self.buckets != value):
-
-            hashedkey = (hashedkey + 1) % self.size
+            hashedkey = (hashedkey + 1) % self.capacity
             i = 0
-            while self.buckets[hashedkey] != None and i < self.size:
-                hashedkey = (hashedkey + 1) % self.size
-                i+=1
-            if self.buckets[hashedkey] == None:
+            while self.buckets[hashedkey] != None and i < self.capacity:
+                hashedkey = (hashedkey + 1) % self.capacity
+                i += 1
+            if self.buckets[hashedkey] is None:
                 self.buckets[hashedkey] = item
 
-
-
     def get(self, id):
-        start= hashedkey = hash(id) % self.size
+        start = hashedkey = hash(id) % self.capacity
         val = None
         done = False
 
@@ -48,33 +67,32 @@ class BankHash:
                 done = True
 
             else:
-                hashedkey = (hashedkey + 1)%self.size
-                if hashedkey ==  start:
+                hashedkey = (hashedkey + 1) % self.capacity
+                if hashedkey == start or self.buckets[hashedkey] is None:
                     done = True
-
-                elif self.buckets[hashedkey] == None:
-                    done = True
-
         return val
 
     def delete(self,removedID):
-        hashedkey = hash(removedID)% self.size
+        hashedkey = hash(removedID)% self.capacity
         i = hashedkey
         prev = self.buckets[hashedkey]
         while prev.getId() != removedID:
             if prev != None:
-                i = (hashedkey + 1) % self.size
+                i = (hashedkey + 1) % self.capacity
                 prev = self.buckets[i]
                 
             else:
                 return None
-
+        
+        toReturn = self.buckets[i]
         self.buckets[i] = None
 
-        while self.buckets[(i + 1) % self.size] != None and hash(self.buckets[(i + 1) % self.size].getId())%self.size == hashedkey:
-            self.buckets[i] = self.buckets [(i+1) % self.size]
-            i = (i + 1) % self.size
+        while self.buckets[(i + 1) % self.capacity] != None and hash(self.buckets[(i + 1) % self.capacity].getId())%self.capacity == hashedkey:
+            self.buckets[i] = self.buckets [(i+1) % self.capacity]
+            i = (i + 1) % self.capacity
             self.buckets[i] = None
+        
+        return toReturn
 
     def __getitem__(self,key):
         return self.get(key)
